fc65f981c3de7b8a393b93a302610dcb8ad8c491
pociello <asierpociello@gmail.com>
2025-03-26 20:33:49 +0100

    Add 'humanScore' and 'computerScore' variables and 'playRound' function
    Implement scoring system and 'playRound' function
    to determine round winner

--- javascript.js
+++ javascript.js
@@ -17,3 +17,29 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let compurterScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+let human = humanChoice.toLowerCase();
+
+if (human ==='rock' && computerChoice === 'rock'){
+    console.log('Draw')
+} else if (human === 'rock' && computerChoice === 'paper') {
+    console.log('You lose! Paper beats Rock')
+} else if (human === 'rock' && computerChoice === 'scissors') {
+    console.log('You win! Rock beats Scissors')
+} else if (human === 'paper' && computerChoice === 'rock') {
+    console.log('You win! Paper beats Rock') 
+} else if (human === 'paper' && computerChoice === 'paper') {
+    console.log('Draw')
+} else if (human === 'paper' && computerChoice === 'scissors') {
+    console.log('You lose! Scissors beats Paper')
+} else if (human === 'scissors' && computerChoice === 'rock') {
+    console.log('You lose! Rock beats Scissors')
+} else if (human === 'scissors' && computerChoice === 'paper') {
+    console.log('You win! Scissors beats Paper')
+} else if (human === 'scissors' && computerChoice === 'scissors') {
+    console.log('Draw') 
+}
+}
+
+playRound(getHumanChoice(), getComputerChoice())
