37206d787c93b1f0e134e5373049289e6993160a
pociello <asierpociello@gmail.com>
2025-03-27 21:49:15 +0100

    Update 'playRound' function
    Add a computer choice message for the player and
    increment in 1 point the score of the winner

--- javascript.js
+++ javascript.js
@@ -16,30 +16,64 @@ function getHumanChoice() {
 }
 
 let humanScore = 0;
-let compurterScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
 let human = humanChoice.toLowerCase();
 
-if (human ==='rock' && computerChoice === 'rock'){
-    console.log('Draw')
-} else if (human === 'rock' && computerChoice === 'paper') {
-    console.log('You lose! Paper beats Rock')
-} else if (human === 'rock' && computerChoice === 'scissors') {
-    console.log('You win! Rock beats Scissors')
-} else if (human === 'paper' && computerChoice === 'rock') {
-    console.log('You win! Paper beats Rock') 
-} else if (human === 'paper' && computerChoice === 'paper') {
-    console.log('Draw')
-} else if (human === 'paper' && computerChoice === 'scissors') {
-    console.log('You lose! Scissors beats Paper')
-} else if (human === 'scissors' && computerChoice === 'rock') {
-    console.log('You lose! Rock beats Scissors')
-} else if (human === 'scissors' && computerChoice === 'paper') {
-    console.log('You win! Scissors beats Paper')
-} else if (human === 'scissors' && computerChoice === 'scissors') {
-    console.log('Draw') 
-}
-}
-
-playRound(getHumanChoice(), getComputerChoice())
+    if (human ==='rock' && computerChoice === 'rock'){
+        console.log('The computer says: Rock')
+        console.log('Draw')
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'rock' && computerChoice === 'paper') {
+        console.log('The computer says: Paper')
+        console.log('You lose! Paper beats Rock')
+        computerScore += 1
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'rock' && computerChoice === 'scissors') {
+        console.log('The computer says: Scissors')
+        console.log('You win! Rock beats Scissors')
+        humanScore += 1
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'paper' && computerChoice === 'rock') {
+        console.log('The computer says: Rock')
+        console.log('You win! Paper beats Rock') 
+        humanScore += 1
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'paper' && computerChoice === 'paper') {
+        console.log('The computer says: Paper')
+        console.log('Draw')
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'paper' && computerChoice === 'scissors') {
+        console.log('The computer says: Scissors')
+        console.log('You lose! Scissors beats Paper')
+        computerScore += 1
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'scissors' && computerChoice === 'rock') {
+        console.log('The computer says: rock')
+        console.log('You lose! Rock beats Scissors')
+        computerScore += 1
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'scissors' && computerChoice === 'paper') {
+        console.log('The computer says: Paper')
+        console.log('You win! Scissors beats Paper')
+        humanScore += 1
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    } else if (human === 'scissors' && computerChoice === 'scissors') {
+        console.log('The computer says: Scissors')
+        console.log('Draw') 
+        console.log('Human Score: ' + humanScore)
+        console.log('Computer Score: ' + computerScore)
+    }
+    }
+    
+    
+   
