5ce889063dd5e51f4d886a06f9da98c21b6938e1
pociello <asierpociello@gmail.com>
2025-04-14 09:29:04 +0200

    Finish game full funtionality

--- javascript.js
+++ javascript.js
@@ -5,104 +5,7 @@ let p1 = document.querySelector("#p1");
 let p2 = document.querySelector("#p2");
 let p3 = document.querySelector("#p3");
 let p4 = document.querySelector("#p4");
-
-
-
-function getComputerChoice() {
-    let x = Math.random()
-
-    if (x < 0.33) {
-        return 'rock'
-    } else if (x < 0.66) {
-        return 'paper'
-    } else {
-        return 'scissors'
-    }
-}
-
-function getHumanChoice() {
-    return prompt('rock, paper or scissors?')
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-
-    let human = humanChoice.toLowerCase();
-
-    if (human ==='rock' && computerChoice === 'rock'){
-
-        p1.textContent = 'The computer says: Rock';
-        p2.textContent = 'Draw';
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'rock' && computerChoice === 'paper') {
-        p1.textContent = 'The computer says: Paper';
-        p2.textContent = 'You lose! Paper beats Rock';
-        computerScore += 1
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'rock' && computerChoice === 'scissors') {
-        p1.textContent = 'The computer says: Scissors';
-        p2.textContent = 'You win! Rock beats Scissors';
-        humanScore += 1
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'paper' && computerChoice === 'rock') {
-        p1.textContent = 'The computer says: Rock';
-        p2.textContent = 'You win! Paper beats Rock';
-        humanScore += 1
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'paper' && computerChoice === 'paper') {
-        p1.textContent = 'The computer says: Paper';
-        p2.textContent = 'Draw';
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'paper' && computerChoice === 'scissors') {
-        p1.textContent = 'The computer says: Scissors';
-        p2.textContent = 'You lose! Scissors beats Paper';
-        computerScore += 1
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'scissors' && computerChoice === 'rock') {
-        p1.textContent = 'The computer says: rock';
-        p2.textContent = 'You lose! Rock beats Scissors';
-        computerScore += 1
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'scissors' && computerChoice === 'paper') {
-        p1.textContent = 'The computer says: Paper';
-        p2.textContent = 'You win! Scissors beats Paper';
-        humanScore += 1
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-
-    } else if (human === 'scissors' && computerChoice === 'scissors') {
-        p1.textContent = 'The computer says: Scissors';
-        p2.textContent = 'Draw';
-        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
-    }
-    }
-
-
-/*function playGame() {
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-  
-    if (humanScore === computerScore) {
-        console.log('FINAL RESULT: DRAW')
-    } else if (humanScore > computerScore) {
-        console.log('FINAL RESULT: HUMAN WINS')
-    } else {
-        console.log('FINAL RESULT: COMPUTER WINS')
-    }
-}
-playGame()
-*/ 
+let h2 = document.querySelector('h2');
 
 const rockBtn = document.querySelector("#rock");
 rockBtn.addEventListener("click", () => {
@@ -119,3 +22,94 @@ scissorsBtn.addEventListener("click", () => {
     playRound('scissors', getComputerChoice())
 })
 
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+    let x = Math.random()
+
+    if (x < 0.33) {
+        return 'rock'
+    } else if (x < 0.66) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (h2.textContent) {
+        h2.textContent = '';
+        humanScore = 0;
+        computerScore = 0;
+    }
+
+    if (humanChoice ==='rock' && computerChoice === 'rock'){
+
+        p1.textContent = 'The computer says: Rock';
+        p2.textContent = 'Draw';
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        p1.textContent = 'The computer says: Paper';
+        p2.textContent = 'You lose! Paper beats Rock';
+        computerScore += 1
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        p1.textContent = 'The computer says: Scissors';
+        p2.textContent = 'You win! Rock beats Scissors';
+        humanScore += 1
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        p1.textContent = 'The computer says: Rock';
+        p2.textContent = 'You win! Paper beats Rock';
+        humanScore += 1
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+        p1.textContent = 'The computer says: Paper';
+        p2.textContent = 'Draw';
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        p1.textContent = 'The computer says: Scissors';
+        p2.textContent = 'You lose! Scissors beats Paper';
+        computerScore += 1
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        p1.textContent = 'The computer says: rock';
+        p2.textContent = 'You lose! Rock beats Scissors';
+        computerScore += 1
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        p1.textContent = 'The computer says: Paper';
+        p2.textContent = 'You win! Scissors beats Paper';
+        humanScore += 1
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+
+    } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+        p1.textContent = 'The computer says: Scissors';
+        p2.textContent = 'Draw';
+        p3.textContent = `Human Score: ${humanScore} || Computer Score: ${computerScore}`;
+    }
+
+    if (humanScore == 5 || computerScore == 5) {
+        finalResult(humanScore, computerScore)
+    };
+    }
+
+function finalResult(humanScore, computerScore) {
+    if (humanScore === computerScore) {
+        h2.textContent = 'FINAL RESULT: DRAW';
+    } else if (humanScore > computerScore) {
+        h2.textContent = 'FINAL RESULT: HUMAN WINS';
+    } else {
+        h2.textContent = 'FINAL RESULT: COMPUTER WINS'
+    }
+}
